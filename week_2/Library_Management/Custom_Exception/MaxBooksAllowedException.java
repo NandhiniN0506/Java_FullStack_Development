8c3ced300040f187cefbacd122afc78a9ad58a8b
NandhiniN0506 <157806111+NandhiniN0506@users.noreply.github.com>
2025-02-11 13:16:45 +0530

    Add files via upload

---
+++ week_2/Library_Management/Custom_Exception/MaxBooksAllowedException.java
@@ -0,0 +1,9 @@
+package week_2.Library_Management.Custom_Exception;
+
+public class MaxBooksAllowedException extends Exception{
+	
+	public MaxBooksAllowedException(String message) {
+		super(message);
+	}
+
+}
